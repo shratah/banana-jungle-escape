f55ba2a04fd50b4b9cdefcf23480c7a4f2a8e512
SINTHUJAgit <kasisinthu8@gmail.com>
2026-02-22 21:13:19 +0530

    Ad registration system : db.php,register.php,register.html

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+include "db.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful! <a href='login.php'>Login here</a>";
+    } else {
+        echo "Error: Username may already exist.";
+    }
+}
+?>
+
+<h2>Register</h2>
+<form method="POST">
+    Username:<br>
+    <input type="text" name="username" required><br><br>
+
+    Password:<br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">Register</button>
+</form>
